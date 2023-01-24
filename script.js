a0130d88231020e70ad82a3157624706cf1e4dc3
RaulBBenzi <raulbiscaro@gmail.com>
2023-01-24 19:35:48 -0300

    new commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+function send(clickbutton) {
+  switch (clickbutton) {
+    case "instagram":
+      window.open("https://www.instagram.com/raul_benzi/");
+      break;
+    case "twitter":
+      window.open("https://twitter.com/RaulRakko");
+      break;
+    case "github":
+      window.open("https://github.com/RaulBBenzi");
+      break;
+    case "linkedin":
+      window.open("https://www.linkedin.com/in/raul-benzi-017ab4238/");
+      break;
+  }
+}
